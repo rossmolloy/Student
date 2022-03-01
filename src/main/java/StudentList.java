8a8297626e5be1796e9bd7ee9db63bef20a355e6
ROSS MOLLOY - STUDENT <G00359442@gmit.ie>
2022-03-01 09:42:14 +0000

    Add StudentList and StudentListTest

---
+++ src/main/java/StudentList.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class StudentList {
+    private ArrayList<Student> students;
+
+    public StudentList(ArrayList<Student> students) {
+        this.students = students;
+    }
+}
